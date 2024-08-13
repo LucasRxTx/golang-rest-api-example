b4bc10e99257fbed7371272ed45a4a56a83280e9
LucasRxTx <LucasRxTx@LucasScott.com>
2024-08-13 10:51:05 +0200

    Refactors transaction handling

--- database/database.go
+++ database/database.go
@@ -10,3 +10,54 @@ func GetConnection() (*sql.DB, error) {
 	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", settings.DB_HOST, settings.DB_PORT, settings.DB_USER, settings.DB_PASSWORD, settings.DB_DATABASE)
 	return sql.Open("postgres", psqlconn)
 }
+
+type Transaction struct {
+	Db          *sql.DB
+	Transaction func(tx *sql.Tx) error
+	OnError     func(err error) error
+}
+
+func (t Transaction) Execute() error {
+	tx, err := t.Db.Begin()
+	if err != nil {
+		return t.OnError(err)
+	}
+
+	defer tx.Rollback()
+
+	err = t.Transaction(tx)
+	if err != nil {
+		return t.OnError(err)
+	}
+
+	err = tx.Commit()
+
+	return err
+}
+
+type TransactionWithValue[T any] struct {
+	Db          *sql.DB
+	Transaction func(tx *sql.Tx) (T, error)
+	OnError     func(err error) (T, error)
+}
+
+func (t TransactionWithValue[T]) Execute() (T, error) {
+	tx, err := t.Db.Begin()
+	if err != nil {
+		return t.OnError(err)
+	}
+
+	defer tx.Rollback()
+
+	value, err := t.Transaction(tx)
+	if err != nil {
+		return t.OnError(err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return t.OnError(err)
+	}
+
+	return value, nil
+}
--- services/user_service.go
+++ services/user_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"database/sql"
+	"rest-api/database"
 	"rest-api/domain"
 	"rest-api/repository"
 
@@ -26,177 +27,118 @@ type UserService struct {
 }
 
 func (userService *UserService) GetAllFriends(userId string) ([]domain.UserFriend, error) {
+	transaction := func(tx *sql.Tx) ([]domain.UserFriend, error) {
+		return userService.UserFreindsRepo.GetAllForUserId(tx, userId)
+	}
+
 	fail := func(err error) ([]domain.UserFriend, error) {
 		return []domain.UserFriend{}, err
 	}
 
-	tx, err := userService.Db.Begin()
-	if err != nil {
-		return fail(err)
-	}
-
-	defer tx.Rollback()
-
-	userFriends, err := userService.UserFreindsRepo.GetAllForUserId(tx, userId)
-	if err != nil {
-		return fail(err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return fail(err)
-	}
-
-	return userFriends, nil
+	return database.TransactionWithValue[[]domain.UserFriend]{
+		Db:          userService.Db,
+		Transaction: transaction,
+		OnError:     fail,
+	}.Execute()
 }
 
 func (userService *UserService) UpdateFriends(id string, friends []string) error {
-	tx, err := userService.Db.Begin()
-	if err != nil {
+	transaction := func(tx *sql.Tx) error {
+		return userService.UserFreindsRepo.Update(tx, id, friends)
+	}
+
+	fail := func(err error) error {
 		return err
 	}
 
-	defer tx.Rollback()
-	err = userService.UserFreindsRepo.Update(tx, id, friends)
-	if err != nil {
-		return err
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return database.Transaction{
+		Db:          userService.Db,
+		Transaction: transaction,
+		OnError:     fail,
+	}.Execute()
 }
 
 func (userService *UserService) GetAll() ([]domain.User, error) {
+	transaction := func(tx *sql.Tx) ([]domain.User, error) {
+		return userService.UserRepo.GetAll(tx)
+	}
+
 	fail := func(err error) ([]domain.User, error) {
 		return []domain.User{}, err
 	}
 
-	tx, err := userService.Db.Begin()
-	if err != nil {
-		return fail(err)
-	}
-
-	defer tx.Rollback()
-
-	users, err := userService.UserRepo.GetAll(tx)
-	if err != nil {
-		return fail(err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return fail(err)
-	}
-
-	return users, nil
+	return database.TransactionWithValue[[]domain.User]{
+		Db:          userService.Db,
+		Transaction: transaction,
+		OnError:     fail,
+	}.Execute()
 }
 
 func (userService *UserService) GetUserById(id string) (domain.User, error) {
+	transaction := func(tx *sql.Tx) (domain.User, error) {
+		return userService.UserRepo.GetById(tx, id)
+	}
+
 	fail := func(err error) (domain.User, error) {
 		return domain.User{}, err
 	}
 
-	tx, err := userService.Db.Begin()
-	if err != nil {
-		return fail(err)
-	}
-
-	defer tx.Rollback()
-
-	user, err := userService.UserRepo.GetById(tx, id)
-	if err != nil {
-		return fail(err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return fail(err)
-	}
-
-	return user, nil
+	return database.TransactionWithValue[domain.User]{
+		Db:          userService.Db,
+		Transaction: transaction,
+		OnError:     fail,
+	}.Execute()
 }
 
 func (userService *UserService) CreateUser(name string) (uuid.UUID, error) {
+	transaction := func(tx *sql.Tx) (uuid.UUID, error) {
+		return userService.UserRepo.Create(tx, name)
+	}
+
 	fail := func(err error) (uuid.UUID, error) {
 		return uuid.UUID{}, err
 	}
 
-	tx, err := userService.Db.Begin()
-	if err != nil {
-		return fail(err)
-	}
-
-	defer tx.Rollback()
-
-	uuid, err := userService.UserRepo.Create(tx, name)
-	if err != nil {
-		return fail(err)
-	}
-
-	_, err = userService.GameStateRepo.Create(tx, uuid.String(), 0, 0)
-	if err != nil {
-		return fail(err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return fail(err)
-	}
-
-	return uuid, nil
+	return database.TransactionWithValue[uuid.UUID]{
+		Db:          userService.Db,
+		Transaction: transaction,
+		OnError:     fail,
+	}.Execute()
 }
 
 func (userService *UserService) GetGameState(userId string) (domain.GameState, error) {
+	transaction := func(tx *sql.Tx) (domain.GameState, error) {
+		return userService.GameStateRepo.GetByUserId(tx, userId)
+	}
+
 	fail := func(err error) (domain.GameState, error) {
 		return domain.GameState{}, err
 	}
 
-	tx, err := userService.Db.Begin()
-	if err != nil {
-		return fail(err)
-	}
-
-	defer tx.Rollback()
-
-	gameState, err := userService.GameStateRepo.GetByUserId(tx, userId)
-	if err != nil {
-		return fail(err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return fail(err)
-	}
-
-	return gameState, nil
+	return database.TransactionWithValue[domain.GameState]{
+		Db:          userService.Db,
+		Transaction: transaction,
+		OnError:     fail,
+	}.Execute()
 }
 
 func (userService *UserService) UpdateGameState(id string, gamesPlayed int, score int) error {
-	tx, err := userService.Db.Begin()
-	if err != nil {
+	transaction := func(tx *sql.Tx) error {
+		gameState, err := userService.GameStateRepo.GetByUserId(tx, id)
+		if err != nil {
+			return err
+		}
+
+		return userService.GameStateRepo.Update(tx, gameState.Id.String(), gamesPlayed, score)
+	}
+
+	fail := func(err error) error {
 		return err
 	}
 
-	defer tx.Rollback()
-
-	gameState, err := userService.GameStateRepo.GetByUserId(tx, id)
-	if err != nil {
-		return err
-	}
-
-	err = userService.GameStateRepo.Update(tx, gameState.Id.String(), gamesPlayed, score)
-	if err != nil {
-		return err
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return database.Transaction{
+		Db:          userService.Db,
+		Transaction: transaction,
+		OnError:     fail,
+	}.Execute()
 }
